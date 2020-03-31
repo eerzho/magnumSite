570b1877a7addb0400030babc2b1465e8bc3715f
eerzho <eerzho@gmail.com>
2020-04-01 04:35:54 +0600

    йцу

--- src/main/java/kz/iitu/javaee/ilyasProject/controllers/MainController.java
+++ src/main/java/kz/iitu/javaee/ilyasProject/controllers/MainController.java
@@ -1,14 +1,16 @@
 package kz.iitu.javaee.ilyasProject.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-//    цукцукцук
+
     @GetMapping(path = "/")
     public String index(Model model){
+        model.addAttribute("classActiveSettingsIndexPage", "active");
         return "index";
     }
 
